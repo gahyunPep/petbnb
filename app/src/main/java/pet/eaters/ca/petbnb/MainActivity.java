bb063034babdb6ef229a50f2e511389250150ce5
Amy Nguyen <thuylinh11t@gmail.com>
2019-02-03 22:44:29 -0800

    implement navigation drawer in main activity

--- app/src/main/java/pet/eaters/ca/petbnb/MainActivity.java
+++ app/src/main/java/pet/eaters/ca/petbnb/MainActivity.java
@@ -2,10 +2,14 @@ package pet.eaters.ca.petbnb;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.navigation.NavigationView;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import pet.eaters.ca.petbnb.pets.postfrom.PetPostFormActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     Button mMapBtn;
     Button mPetDtlBtn;
     Button mPostFormBtn;
+    private DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         mMapBtn = findViewById(R.id.mapBtn);
         mPetDtlBtn = findViewById(R.id.petDetailBtn);
         mPostFormBtn = findViewById(R.id.postFormBtn);
-
+        mDrawerLayout = findViewById(R.id.drawer_layout);
 
         mMapBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +49,23 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, PetPostFormActivity.class));
             }
         });
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
 
     }
 }
