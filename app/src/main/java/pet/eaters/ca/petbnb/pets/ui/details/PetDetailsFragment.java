8989fbcd9f7e62c129420ba9c91a9428c359f832
Amy Nguyen <thuylinh11t@gmail.com>
2019-03-07 19:44:28 -0800

    Generate QR Code from petID

--- app/src/main/java/pet/eaters/ca/petbnb/pets/ui/details/PetDetailsFragment.java
+++ app/src/main/java/pet/eaters/ca/petbnb/pets/ui/details/PetDetailsFragment.java
@@ -7,15 +7,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.google.android.material.tabs.TabLayout;
-
 import java.util.List;
-
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.viewpager.widget.ViewPager;
+import pet.eaters.ca.petbnb.QRCodeGenAndReader;
 import pet.eaters.ca.petbnb.R;
 import pet.eaters.ca.petbnb.core.Result;
 import pet.eaters.ca.petbnb.pets.data.Pet;
@@ -41,6 +39,8 @@ public class PetDetailsFragment extends Fragment {
     private Integer genderData;
     private String petSize;
     private Integer petSizeData;
+    private ImageView QRCodeImg;
+    QRCodeGenAndReader petQRCodeImg;
 
 
     public static PetDetailsFragment newInstance(String petId) {
@@ -71,6 +71,7 @@ public class PetDetailsFragment extends Fragment {
         petPhone = rootView.findViewById(R.id.pet_phone);
         petCity = rootView.findViewById(R.id.pet_city);
         petGender = rootView.findViewById(R.id.pet_gender);
+        QRCodeImg = rootView.findViewById(R.id.QRImageView);
 
         imageSlideIndicator = rootView.findViewById(R.id.image_slide_indicator);
         imageSlideIndicator.setupWithViewPager(viewPager, true);
@@ -114,7 +115,8 @@ public class PetDetailsFragment extends Fragment {
                 viewPager.setAdapter(viewPagerAdapter);
             }
         });
-       return rootView;
+        petQRCodeImg = new QRCodeGenAndReader(QRCodeImg, petId);
+        petQRCodeImg.generateQRCode();
+        return rootView;
     }
-
 }
