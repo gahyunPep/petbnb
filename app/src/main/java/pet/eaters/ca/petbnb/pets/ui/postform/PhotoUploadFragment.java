095a917b571fde74e7952bc27a0f18e712a33e62
nithilch <nithilaksha@gmail.com>
2019-03-13 12:00:55 -0700

    implemented uploading to FireStore, permissions is still pending

--- app/src/main/java/pet/eaters/ca/petbnb/pets/ui/postform/PhotoUploadFragment.java
+++ app/src/main/java/pet/eaters/ca/petbnb/pets/ui/postform/PhotoUploadFragment.java
@@ -1,30 +1,36 @@
 package pet.eaters.ca.petbnb.pets.ui.postform;
 
-import androidx.lifecycle.ViewModelProviders;
-
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
-
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.loader.content.CursorLoader;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import pet.eaters.ca.petbnb.R;
+import pet.eaters.ca.petbnb.pets.data.PhotoStorage;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -33,6 +39,7 @@ public class PhotoUploadFragment extends Fragment {
     private PhotoUploadViewModel mViewModel;
     private Button addCameraBtn;
     private Button addGalleryBtn;
+    private Button uploadBtn;
     private final int REQUEST_IMAGE_CAPTURE = 1;
     private final int REQUEST_LOAD_GALLERY = 2;
 
@@ -40,14 +47,16 @@ public class PhotoUploadFragment extends Fragment {
         return new PhotoUploadFragment();
     }
 
-    List<Bitmap> bitmapList;
+    List<Bitmap> imageList;
+    List<String> imagePathList;
     PhotoUploadAdapter photoUploadAdapter;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.photo_upload_fragment, container, false);
-        bitmapList = new ArrayList<>();
+        imageList = new ArrayList<>();
+        imagePathList = new ArrayList<>();
 
         RecyclerView photosView = view.findViewById(R.id.photosView);
         photosView.setLayoutManager(new GridLayoutManager(view.getContext(), 3));
@@ -68,7 +77,17 @@ public class PhotoUploadFragment extends Fragment {
             }
         });
 
-        photoUploadAdapter = new PhotoUploadAdapter(bitmapList);
+        uploadBtn = view.findViewById(R.id.btn_upload);
+        uploadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadToFireStore();
+                Toast.makeText(getContext(),"Upload Successful", Toast.LENGTH_SHORT).show();
+                getFragmentManager().popBackStack();
+            }
+        });
+
+        photoUploadAdapter = new PhotoUploadAdapter(imageList);
         photosView.setAdapter(photoUploadAdapter);
         return view;
     }
@@ -91,6 +110,12 @@ public class PhotoUploadFragment extends Fragment {
         startActivityForResult(photoPickerIntent, REQUEST_LOAD_GALLERY);
     }
 
+    public void uploadToFireStore() {
+        String samplePetId = "t5jVufrc7Nj1MJXZqjLs";
+        PhotoStorage photoStorage = new PhotoStorage();
+        photoStorage.uploadFiles(imagePathList, samplePetId);
+    }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         switch (requestCode) {
@@ -98,6 +123,18 @@ public class PhotoUploadFragment extends Fragment {
                 if (resultCode == RESULT_OK) {
                     Bundle extras = data.getExtras();
                     Bitmap bitmapFromCamera = (Bitmap) extras.get("data");
+                    File imageFile = null;
+                    try {
+                        imageFile = createImageFile();
+                        FileOutputStream iOut = new FileOutputStream(imageFile);
+                        bitmapFromCamera.compress(Bitmap.CompressFormat.JPEG, 100, iOut);
+                        iOut.flush();
+                        iOut.close();
+                    } catch (IOException ex) {
+                        Log.e("IMAGE FILE", ex.toString());
+                    }
+                    imagePathList.add(imageFile.getAbsolutePath());
+                    Log.v("CAMERA PATH", imageFile.getAbsolutePath());
                     addToBitmapList(bitmapFromCamera);
                 }
                 break;
@@ -110,16 +147,49 @@ public class PhotoUploadFragment extends Fragment {
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
+                    Log.v("GALLERY PATH", getPathFromUri(imageUri));
+                    imagePathList.add(getPathFromUri(imageUri));
                     addToBitmapList(bitmapFromGallery);
                 }
                 break;
 
         }
-        photoUploadAdapter.updateArray(bitmapList);
+        photoUploadAdapter.updateArray(imageList);
         photoUploadAdapter.notifyDataSetChanged();
     }
 
     public void addToBitmapList(Bitmap bitmap) {
-        bitmapList.add(bitmap);
+        imageList.add(bitmap);
+    }
+    
+    public String getPathFromUri(Uri contentUri) {
+        String[] proj = {MediaStore.Images.Media.DATA};
+        String result = null;
+
+        CursorLoader cursorLoader = new CursorLoader(
+                this.getContext(),
+                contentUri, proj, null, null, null);
+        Cursor cursor = cursorLoader.loadInBackground();
+
+        if (cursor != null) {
+            int column_index =
+                    cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            result = cursor.getString(column_index);
+        }
+        return result;
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = this.getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        return image;
     }
 }
