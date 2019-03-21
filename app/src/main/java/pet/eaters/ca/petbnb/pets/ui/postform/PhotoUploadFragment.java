30223981579063f32ff26d60d1c225dba53a2500
Roman Lee <rokanank@gmail.com>
2019-03-20 19:07:30 -0700

    code cleanups

--- app/src/main/java/pet/eaters/ca/petbnb/pets/ui/postform/PhotoUploadFragment.java
+++ app/src/main/java/pet/eaters/ca/petbnb/pets/ui/postform/PhotoUploadFragment.java
@@ -1,8 +1,6 @@
 package pet.eaters.ca.petbnb.pets.ui.postform;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -20,13 +18,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.loader.content.CursorLoader;
@@ -46,12 +42,10 @@ public class PhotoUploadFragment extends Fragment {
     private RecyclerView photosRecyclerView;
     private final int REQUEST_IMAGE_CAPTURE = 1;
     private final int REQUEST_LOAD_GALLERY = 2;
-    private final int REQUEST_ASK_PERMISSION = 3;
 
-    List<Bitmap> imageList;
-    List<String> imagePathList;
-    PhotoUploadAdapter photoUploadAdapter = new PhotoUploadAdapter();
-    final String[] PERMISSIONS = {Manifest.permission.READ_EXTERNAL_STORAGE};
+    private List<Bitmap> imageList;
+    private List<String> imagePathList;
+    private PhotoUploadAdapter photoUploadAdapter = new PhotoUploadAdapter();
 
     public static PhotoUploadFragment newInstance() {
         return new PhotoUploadFragment();
@@ -65,7 +59,6 @@ public class PhotoUploadFragment extends Fragment {
         imageList = new ArrayList<>();
         imagePathList = new ArrayList<>();
 
-        checkPermissions();
 
         photosRecyclerView = view.findViewById(R.id.photosView);
         photosRecyclerView.setLayoutManager(new GridLayoutManager(view.getContext(), 3));
@@ -140,16 +133,14 @@ public class PhotoUploadFragment extends Fragment {
 
     private void loadGallery(Intent data) {
         Uri imageUri = data.getData();
-        Bitmap bitmapFromGallery = null;
         try {
-            bitmapFromGallery = MediaStore.Images.Media.getBitmap(this.getActivity().getContentResolver(), imageUri);
+            Bitmap bitmapFromGallery = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imageUri);
+            Log.v("GALLERY PATH", getPathFromUri(imageUri));
+            imagePathList.add(getPathFromUri(imageUri));
+            addToBitmapList(bitmapFromGallery);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Log.v("GALLERY PATH", getPathFromUri(imageUri));
-        imagePathList.add(getPathFromUri(imageUri));
-        addToBitmapList(bitmapFromGallery);
-        photoUploadAdapter.updateList(imageList);
     }
 
     private void imageCapture(final Intent data) {
@@ -158,32 +149,30 @@ public class PhotoUploadFragment extends Fragment {
             public void run() {
                 Bundle extras = data.getExtras();
                 Bitmap bitmapFromCamera = (Bitmap) extras.get("data");
-                File imageFile = null;
                 try {
-                    imageFile = createImageFile();
+                    File imageFile = createImageFile();
                     FileOutputStream iOut = new FileOutputStream(imageFile);
                     bitmapFromCamera.compress(Bitmap.CompressFormat.JPEG, 100, iOut);
                     iOut.flush();
                     iOut.close();
+                    imagePathList.add(imageFile.getAbsolutePath());
+                    final Bitmap bitmap = bitmapFromCamera;
+                    photosRecyclerView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            addToBitmapList(bitmap);
+                        }
+                    });
                 } catch (IOException ex) {
                     Log.e("IMAGE FILE", ex.toString());
                 }
-                imagePathList.add(imageFile.getAbsolutePath());
-                final Bitmap bitmap = bitmapFromCamera;
-                photosRecyclerView.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        addToBitmapList(bitmap);
-                        photoUploadAdapter.updateList(imageList);
-                    }
-                });
-
             }
         }).start();
     }
 
     public void addToBitmapList(Bitmap bitmap) {
         imageList.add(bitmap);
+        photoUploadAdapter.updateList(imageList);
     }
 
     public String getPathFromUri(Uri contentUri) {
@@ -207,51 +196,12 @@ public class PhotoUploadFragment extends Fragment {
     private File createImageFile() throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = this.getContext().getFilesDir();
+        File storageDir = getContext().getFilesDir();
 
-        File image = File.createTempFile(
+        return File.createTempFile(
                 imageFileName,  /* prefix */
                 ".jpg",         /* suffix */
                 storageDir      /* directory */
         );
-        return image;
-    }
-
-    // Source: https://developer.here.com/documentation/android-premium/dev_guide/topics/request-android-permissions.html
-    private void checkPermissions() {
-        final List<String> missingPermissions = new ArrayList<>();
-
-        for (String permission : PERMISSIONS) {
-            final int result = ContextCompat.checkSelfPermission(getContext(), permission);
-            if (result != PackageManager.PERMISSION_GRANTED) {
-                missingPermissions.add(permission);
-            }
-        }
-
-        if (!missingPermissions.isEmpty()) {
-            final String[] permissions = missingPermissions.toArray(new String[missingPermissions.size()]);
-            requestPermissions(permissions, REQUEST_ASK_PERMISSION);
-        } else {
-            final int[] grantResults = new int[PERMISSIONS.length];
-            Arrays.fill(grantResults, PackageManager.PERMISSION_GRANTED);
-            onRequestPermissionsResult(REQUEST_ASK_PERMISSION, PERMISSIONS, grantResults);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_ASK_PERMISSION:
-                for (int index = permissions.length - 1; index >= 0; --index) {
-                    if (grantResults[index] != PackageManager.PERMISSION_GRANTED) {
-                        // exit fragment if permission not granted
-                        Toast.makeText(getContext(), "Required permission '" + permissions[index]
-                                + "' not granted, exiting", Toast.LENGTH_LONG).show();
-                        return;
-                    }
-                }
-                // all permissions granted
-                return;
-        }
     }
 }
