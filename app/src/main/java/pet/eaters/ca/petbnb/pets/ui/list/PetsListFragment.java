b34a99d9ab0383bb8ce1b967baa0b3f088775777
nithilch <nithilaksha@gmail.com>
2019-03-21 14:41:18 -0700

    Implemented Swipe Refresh Layout

--- app/src/main/java/pet/eaters/ca/petbnb/pets/ui/list/PetsListFragment.java
+++ app/src/main/java/pet/eaters/ca/petbnb/pets/ui/list/PetsListFragment.java
@@ -14,6 +14,7 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import pet.eaters.ca.petbnb.R;
 import pet.eaters.ca.petbnb.core.Result;
 import pet.eaters.ca.petbnb.pets.data.Pet;
@@ -24,6 +25,7 @@ public class PetsListFragment extends Fragment implements PetsListAdapter.OnPetC
 
     private PetsListViewModel mViewModel;
     private RecyclerView petsRecyclerView;
+    private SwipeRefreshLayout swipeRefreshLayout;
     PetsListAdapter adapter;
     LiveData<Result<List<Pet>>> pets;
 
@@ -36,6 +38,13 @@ public class PetsListFragment extends Fragment implements PetsListAdapter.OnPetC
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.pets_list_fragment, container, false);
         petsRecyclerView = view.findViewById(R.id.petsRecyclerView);
+        swipeRefreshLayout = view.findViewById(R.id.swipeContainer);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getAllPets();
+            }
+        });
 
         return view;
     }
@@ -47,8 +56,16 @@ public class PetsListFragment extends Fragment implements PetsListAdapter.OnPetC
         adapter = new PetsListAdapter();
         adapter.setPetClickListener(this);
         petsRecyclerView.setAdapter(adapter);
+        getAllPets();
+    }
 
+    @Override
+    public void onPetClicked(Pet item, int position) {
+        PetDetailsFragment fragment = PetDetailsFragment.newInstance(item.getId());
+        getFragmentManager().beginTransaction().replace(R.id.content_frame, fragment).addToBackStack(null).commit();
+    }
 
+    public void getAllPets() {
         //TODO move live data and repository to the ViewModel
         PetsRepository repository = new PetsRepository();
         pets = repository.getPets();
@@ -59,17 +76,12 @@ public class PetsListFragment extends Fragment implements PetsListAdapter.OnPetC
                 List<Pet> data = listResult.getData();
                 if (data != null) {
                     adapter.submitList(data);
+                    swipeRefreshLayout.setRefreshing(false);
                 } else {
                     //TODO error handling
                 }
             }
         });
         // TODO: Use the ViewModel
-    }
-
-    @Override
-    public void onPetClicked(Pet item, int position) {
-        PetDetailsFragment fragment = PetDetailsFragment.newInstance(item.getId());
-        getFragmentManager().beginTransaction().replace(R.id.content_frame, fragment).addToBackStack(null).commit();
     }
 }
