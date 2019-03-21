14c7a6c80654e777ecb16470ed41383012a0dd01
nithilch <nithilaksha@gmail.com>
2019-03-21 13:53:07 -0700

    Get Data from Firebase and set it to RecyclerView items

--- app/src/main/java/pet/eaters/ca/petbnb/pets/ui/list/PetsListAdapter.java
+++ app/src/main/java/pet/eaters/ca/petbnb/pets/ui/list/PetsListAdapter.java
@@ -3,6 +3,13 @@ package pet.eaters.ca.petbnb.pets.ui.list;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.chip.Chip;
+
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -53,13 +60,38 @@ public class PetsListAdapter extends ListAdapter<Pet, PetsListAdapter.PetViewHol
     }
 
     static class PetViewHolder extends RecyclerView.ViewHolder {
+        public TextView petListItemName;
+        public ImageView petListItemImage;
+        public Chip petListItemType;
+        public Chip petListItemAge;
+        public Chip petListItemGender;
+        public Chip petListItemSize;
+
         public PetViewHolder(@NonNull View itemView) {
             super(itemView);
+            petListItemName = itemView.findViewById(R.id.petListItemName);
+            petListItemImage = itemView.findViewById(R.id.petListItemImage);
+            petListItemType = itemView.findViewById(R.id.petListItemType);
+            petListItemAge = itemView.findViewById(R.id.petListItemAge);
+            petListItemGender = itemView.findViewById(R.id.petListItemGender);
+            petListItemSize = itemView.findViewById(R.id.petListItemSize);
         }
 
         public void bind(Pet item, View.OnClickListener clickListener) {
             itemView.setOnClickListener(clickListener);
             //TODO fill with real data
+            petListItemName.setText(item.getName());
+            List<String> imageUrlList = item.getImages();
+            if (imageUrlList.size() > 0) {
+                Glide.with(itemView.getContext())
+                        .load(imageUrlList.get(0))
+                        .into(petListItemImage);
+            }
+//            petListItemType.setText(item.getType());
+////          petListItemAge.setText(item.getAge()+" years old");
+//            petListItemGender.setText(item.getGender());
+//            petListItemSize.setText(item.getSize());
+
         }
     }
 
